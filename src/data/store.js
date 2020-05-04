3bed586ca60e261f13bbdc3158226cb797d66109
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-04 21:09:02 +0200

    add hot reloading

--- src/data/store.js
+++ src/data/store.js
@@ -1,18 +1,23 @@
 import { applyMiddleware, compose, createStore } from "redux";
-import thunkMiddleware from "redux-thunk";
+// import thunkMiddleware from "redux-thunk";
 
-import monitorReducersEnhancer from "./enhancers/monitorReducers";
-import loggerMiddleware from "./middleware/logger";
+// import loggerMiddleware from "./middleware/logger";
 import rootReducer from "./reducers";
 
 export default function configureStore(preloadedState) {
-	const middlewares = [loggerMiddleware, thunkMiddleware];
+	const middlewares = [
+		// loggerMiddleware, thunkMiddleware
+	];
 	const middlewareEnhancer = applyMiddleware(...middlewares);
 
-	const enhancers = [middlewareEnhancer, monitorReducersEnhancer];
+	const enhancers = [middlewareEnhancer];
 	const composedEnhancers = compose(...enhancers);
 
 	const store = createStore(rootReducer, preloadedState, composedEnhancers);
+
+	if (process.env.NODE_ENV !== "production" && module.hot) {
+		module.hot.accept("./reducers", () => store.replaceReducer(rootReducer));
+	}
 
 	return store;
 }
