a1e30afc359963a7fa23cf2ed0c37e2d23e0bf31
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-04 20:41:03 +0200

    add propTypes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { InlineButton, RegularButton } from "./Button.css";
 
-const Button = ({ type, children, ...props }) => {
-	const Component = (() => {
-		switch (type) {
+const Button = ({ variant, children, ...props }) => {
+	const { to } = props;
+	const Component = useMemo(() => {
+		switch (variant) {
 			case "inline":
 				return InlineButton;
 			case "regular":
 				return RegularButton;
 			default:
-				return InlineButton;
+				return RegularButton;
 		}
-	})();
+	}, [variant]);
 
-	return (
-		<>
-			<Component {...props}>{children}</Component>
-		</>
-	);
+	const content = useMemo(() => <Component {...props}>{children}</Component>, [
+		props,
+		children,
+	]);
+
+	return to ? <Link {...props}>{content}</Link> : <>{content}</>;
+};
+
+Button.propTypes = {
+	variant: PropTypes.oneOf(["inline", "regular"]),
 };
 
 export default Button;
